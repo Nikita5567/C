bada9bfe05cf9826326886d3646215e46697b205
Nikita5567 <ilin10988@yandex.ru>
2023-06-16 20:22:28 +0300

    Прошли 2 семинар, решили по ним задачки

---
+++ Seminar2_Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = numberA % 7;
+int numberC = numberA % 23;
+if (numberB == 0 && numberC == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
